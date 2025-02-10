b703d322df46b74c926828ee83afd2112add3150
Rony Moura <ronymmoura@gmail.com>
2025-02-10 18:41:00 -0300

    add ci/cd

--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 import "net/http"
 
 func main() {
-	fs := http.FileServer(http.Dir("client/dist"))
+	fs := http.FileServer(http.Dir("./client/dist"))
 	http.Handle("/", fs)
 
 	http.ListenAndServe(":8081", nil)
